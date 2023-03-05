474295ebb3b25c20d42d6fd507e83fb46c98511f
Kerron Parchment <kerron68@gmail.com>
2023-03-05 14:37:38 +0000

    feat: add desc for movingZeros.hs

--- challenges/movingZeros.hs
+++ challenges/movingZeros.hs
@@ -1,7 +1,8 @@
 {-
-Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+  Write an algorithm that takes an array and moves all of the zeros to the end, 
+  preserving the order of the other elements.
 
-moveZeros [1,2,0,1,0,1,0,3,0,1] -> [1,2,1,1,3,1,0,0,0,0]
+  moveZeros [1,2,0,1,0,1,0,3,0,1] -> [1,2,1,1,3,1,0,0,0,0]
 -}
 
 module MovingZeros (moveZeros) where
