4c8d442f6d879495769c527c2fc09298c686eb78
Kerron Parchment <kerron68@gmail.com>
2023-04-16 11:32:56 +0100

    fix: update module

--- challenges/uniqueNumber.hs
+++ challenges/uniqueNumber.hs
@@ -1,4 +1,4 @@
-module Challenges.UniqueNumber where
+module UniqueNumber where
 {--
 https://www.codewars.com/kata/585d7d5adb20cf33cb000235/solutions/haskell
 There is an array with some numbers. All numbers are equal except for one. Try to find it!
