de5d22832b30234c14a10e537e5ea57b0410f285
Kerron Parchment <kerron68@gmail.com>
2023-04-16 11:00:02 +0100

    feat: doubleInteger.hs

---
+++ challenges/doubleInteger.hs
@@ -0,0 +1,7 @@
+module DoubleInteger where
+
+-- https://www.codewars.com/kata/53ee5429ba190077850011d4/haskell
+-- Code as fast as you can! You need to double the integer and return it.
+
+doubleInteger :: Num a => a -> a
+doubleInteger = (*2)
